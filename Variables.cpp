39c323c55da6b62d500031df9f0db33f46cc7d8c
MayoLayo <l.t05@outlook.com>
2023-05-22 17:18:42 +0200

    Git ignore

---
+++ Variables.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int fiveHundred = 500;
+    cout << fiveHundred;
+    int myNumber;
+    cin >> myNumber;
+    cout << myNumber;
+    return 0;
+}
