65f64f9834708c2d226ace3c3f5eae9945d77d66
MayoLayo <l.t05@outlook.com>
2023-05-22 20:22:58 +0200

    Data types

---
+++ dataTypes.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+    // Whole number
+    int myNumber = 500;
+
+    // Decimal numbers
+    double myDouble = 11.99;
+
+    // Single character
+    char myGrade = 'A';
+    char myCharacter = '%';
+
+    // Boolean values, T/F
+    bool myBool = true;
+    bool yourBool = false;
+
+    // Strings
+    string myName = "Leo";
+    std::string ourPlanet = "Earth";
+    
+    return 0;
+}
