5fd46893a357218fe2413380222553ae5e9d0c52
MayoLayo <l.t05@outlook.com>
2023-05-22 20:22:36 +0200

    In- and outputs

---
+++ outAndIn.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+    cout << "Hello world!" << endl;
+    cout << "This is an example of an output" << endl;
+
+    int yourNumber;
+    cout << "What is your number?" << endl;
+    // This is an example of an input
+    cin >> yourNumber;
+    
+}
