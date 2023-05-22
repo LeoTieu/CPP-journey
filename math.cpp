28f2c1d4e0ea94a010ffb7d8918e3b93d22e20a4
MayoLayo <l.t05@outlook.com>
2023-05-22 20:22:09 +0200

    Math functions in c++

---
+++ math.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+
+
+main()
+{
+    double x;
+    // Addision
+    x = 8 + 5;
+
+    // Subtraction
+    x = 8 - 5;
+
+    // Multiplication
+    x = 8 * 5;
+
+    // Division
+    x = 8 / 5;
+
+    // Exponents, 8^5
+    x = pow(8, 5);
+
+    // Square root
+    x = sqrt(x);
+
+    // Absolute value
+    x = abs(x);
+
+    // Rounded value
+    x = 7.8;
+    x = round(x);
+
+    // Round up and down
+    x = floor(x);
+    x = ceil(x);
+
+    return 0;
+}
