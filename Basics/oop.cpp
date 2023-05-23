f6b91515ee587168498b681422c8fa257c4fab84
MayoLayo <l.t05@outlook.com>
2023-05-23 20:34:27 +0200

    Variables within classes

--- Basics/oop.cpp
+++ Basics/oop.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class cat{
@@ -7,14 +8,23 @@ class cat{
         void meow(){
             cout << "Meow meow meow" << endl;
         }
-        void purr(){
-            cout << "purrr" << endl;
+        void setName(string catName){
+            name = catName;
         }
+        string getName(){
+            return name;
+        }
+    // Only functions within the object
+    // can reach the variable name.
+    private:
+        string name;
 };
 
 int main()
 {
     cat myCat;
     myCat.meow();
-    myCat.purr();
+    myCat.setName("kitten");
+    cout << myCat.getName() << endl;
+
 }
