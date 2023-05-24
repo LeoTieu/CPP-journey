afd8dc04d90b16da797ed916eb2716a6a58d72c1
MayoLayo <l.t05@outlook.com>
2023-05-24 21:43:17 +0200

    Basic loops

--- loops/basicLoops.cpp
+++ loops/basicLoops.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 main()
 {   
-    // Basic for loop
+    // For loop, range
     for(int i = 1; i != 20; i++){
         cout << i << endl;
     }
@@ -12,5 +12,15 @@ main()
     int numbers[] {1,2,3,4,5,6,7,8,9,10};
     for(int number : numbers){
         cout << number << endl;
+    }
+
+    // while true loop + break
+    int x = 0;
+    while(true){
+        x++;
+        // Exits loop if x = 10
+        if(x == 10){
+            break;
+        }
     }
 }
